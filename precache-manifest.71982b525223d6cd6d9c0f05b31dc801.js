eb7d95023fd4912f29c032998a0800a19830219d
IhorTen <tenyachkinigor1992@gmail.com>
2019-04-18 14:39:12 +0300

    Updates

---
+++ precache-manifest.71982b525223d6cd6d9c0f05b31dc801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02a9430b88975cae16fcfcc9cf7b5799",
+    "url": "/the-movie_db-search/static/media/tmdb-search-logo.02a9430b.svg"
+  },
+  {
+    "revision": "196c5eec22e940e511e4",
+    "url": "/the-movie_db-search/static/js/runtime~main.d571b734.js"
+  },
+  {
+    "revision": "7c6c2c00c0e1d6f49d8c",
+    "url": "/the-movie_db-search/static/js/main.7ab9b464.chunk.js"
+  },
+  {
+    "revision": "72c4924512fa7b47aa75",
+    "url": "/the-movie_db-search/static/js/2.0f1b171b.chunk.js"
+  },
+  {
+    "revision": "7c6c2c00c0e1d6f49d8c",
+    "url": "/the-movie_db-search/static/css/main.f71f6b9b.chunk.css"
+  },
+  {
+    "revision": "745c0775bcf1e88daee376effe3281c0",
+    "url": "/the-movie_db-search/index.html"
+  }
+];
